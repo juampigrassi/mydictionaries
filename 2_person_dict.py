6dac9e0a384954562488e3525da749e63d09f357
Juampi <juanpablo_grassimaz1@baylor.edu>
2023-01-30 14:14:39 -0600

    mydic

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,22 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+
+#Name of the second child 
+print(person["children"][1])
+
+#Name of the cat 
+print(type(person['pets']))
+print(person['pets']['cat'])
+
+#List each child 
+
+for i in person['children']:
+    print(i)
+
+#print out the pets in this format, 
+#type of pet:dog, name of pet: fido
+
+for i,j in person['pets'].items():
+    print(f"Type of pet: {i} name of pet: {j}")
